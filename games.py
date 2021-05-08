4d6cd60c18ac4c290c3294aa06eed7200fe75d21
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-05-08 19:35:43 -0300

    starting guessing

---
+++ games.py
@@ -0,0 +1,27 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    games.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/05/08 18:58:26 by anolivei          #+#    #+#              #
+#    Updated: 2021/05/08 19:32:47 by anolivei         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import guessing
+import gallows
+
+print("************************")
+print("*** Choose your game ***")
+print("************************")
+
+print("1 - Guessing\n2 - Gallows")
+
+game = int(input("Which game? "))
+
+if (game == 1):
+	guessing.guessing()
+elif (game == 2):
+	gallows.gallows()
---
+++ guessing.py
@@ -0,0 +1,57 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    guessing.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/05/06 21:38:47 by anolivei          #+#    #+#              #
+#    Updated: 2021/05/08 19:32:39 by anolivei         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import random as rd
+
+def guessing():
+	print("************************")
+	print("*** Playing Guessing ***")
+	print("************************")
+	
+	secret_number = rd.randrange(1,101)
+	guess = int(input("Type a number between 1 and 100: "))
+	points = 1000
+	game_try = 1
+
+	while (guess < 1 or guess > 100):
+		print("Try number {}".format(game_try))
+		points -= abs(secret_number - guess)
+		print("You must choose a number between 1 and 100!")
+		guess = int(input("Type a number between 1 and 100: "))
+		game_try += 1
+
+	right = guess == secret_number
+
+	while (right == 0):
+		if (guess < 1 or guess > 100):
+			print("You must choose a number between 1 and 100!")
+			points -= abs(secret_number - guess)
+			continue
+		higher	= guess > secret_number
+		lower	= guess < secret_number
+		if (higher):
+			print("Try number {}".format(game_try))
+			points -= abs(secret_number - guess)
+			guess = input("It isn't the secret number, try a lower: ")
+		elif (lower):
+			print("Try number {}".format(game_try))
+			points -= abs(secret_number - guess)
+			guess = input("It isn't the secret number, try a higher one: ")
+		guess = int(guess)
+		right = guess == secret_number
+		game_try += 1
+		
+	if (right):
+		print("Yay, you're right, the secret number was {}, you spent {} tries to discovery it, your score is {}".format(secret_number, game_try, points))
+
+if(__name__ == "__main__"):
+	guessing()
