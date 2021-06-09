4ec2a01ff6df6cdff17b91f89776186ce686b641
Angélica Oliveira Miranda <59845902+anolivei@users.noreply.github.com>
2021-06-09 13:04:52 -0300

    starting gallows

--- python/gallows.py
+++ python/gallows.py
@@ -6,7 +6,7 @@
 #    By: anolivei <anolivei@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/05/08 19:16:48 by anolivei          #+#    #+#              #
-#    Updated: 2021/05/08 19:34:56 by anolivei         ###   ########.fr        #
+#    Updated: 2021/06/09 13:03:27 by anolivei         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,24 @@ def gallows():
 	print("***********************")
 	print("*** Playing Gallows ***")
 	print("***********************")
+	
+	secret_word = "vogons".lower()
+	correct_letters = ["_", "_", "_", "_", "_", "_"]
+	print(correct_letters)
+	dead = False
+	won = False
+	while(not dead and not won):
+		guessing = input("Choose a letter: ").lower().strip()
+		i = 0
+		for letter in secret_word:
+			if(guessing == letter):
+				print("Letter {} found in position {}".format(guessing, i))
+				correct_letters[i] = guessing
+				print(correct_letters)
+			i += 1
+		print("Playing...")
 
-	print("hello")
+	print("Game Over")
 
 if(__name__ == "__main__"):
 	gallows()
